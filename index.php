3184bd37bdc1787fa7c664098269d00d5633894f
nandogt21 <nandogt21@users.noreply.github.com>
2018-06-12 23:03:54 -0300

    aquivos do projetoPhone

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>phoneLocation</title>
+        <link rel="stylesheet" href="index.css">
+    </head>
+    <style type="text/css">
+        body{
+     background-color:#11a6d4;
+    }
+    #pagina{
+        position:absolute;
+        font-size:450%;
+        color:white;
+        left:36%;
+        top:10%;
+           
+    }
+       #login-box-interno{
+           width:440px;
+           height:290px;
+           top:200px;
+           left:500px;
+           position:absolute;
+           background-color:#fdfdfd;
+           margin:140px auto 0 px;
+           border-radius:5px;
+           box-shadow:5px 5px 5px black;
+           overflow:hidden;
+      }
+       #cadastro{
+           position:absolute;
+           left:70%;
+           top:85%
+          }
+       
+    </style>
+    <body>
+        <div id="pagina"><center>PhoneLocation</center></div>
+        
+        <div id="login-box-interno">
+            <center><h1>Faça o Login</h1></center>
+    <form action="receber" method="post">
+        <h3 id="Usuario">Usuario:<h3><input type="text" id="escrever" size="20"><br><br>
+       <h3 id="Senha">Senha:</h3><input type="password" id="senha1" size="20">
+    <input type="button" value="ENVIAR" id="enviar" size="15">
+    <input type="button" value="CANCELAR" id="cancelar" size="15">
+    
+</form>
+            <div id="cadastro">
+		<a class="txt1" href="#">
+	            Cadastra-se
+		</a>
+        </div>
+                
+        
+    </body>
+</html>
